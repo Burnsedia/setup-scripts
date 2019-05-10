81a0994c6ab8074817004de1ac717ea1f7bb7711
Burnsedia <bburnsed1995@gmail.com>
2019-05-10 18:38:21 -0400

    Made a dev env setup script

---
+++ workspace-setup.sh
@@ -0,0 +1,18 @@
+#!usr/bin/bash
+
+sudo snap install --classic beta  blender
+sudo snap install krita
+sudo snap install gimp
+sudo snap install obs-studio 
+sudo snap install spotify
+sudo snap install gitter-desktop 
+sudo snap install godot
+sudo apt install lmms
+
+sudo apt install git
+sudo apt install npm
+sudo apt install pip && pip3
+sudo apt install cython
+sudo apt install nim
+sudo apt install emacs-25
+
