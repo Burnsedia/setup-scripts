7b9cedc42449c99d66b7ab76664d5d98aa62dd02
Bailey <burnsedia@pop-os.localdomain>
2020-04-24 20:07:15 -0400

    updated the script

--- workspace-setup.sh
+++ workspace-setup.sh
@@ -1,6 +1,6 @@
 #!usr/bin/bash
 
-sudo snap install --classic beta  blender
+sudo snap instal  blender
 sudo snap install krita
 sudo snap install gimp
 sudo snap install obs-studio 
@@ -9,10 +9,16 @@ sudo snap install gitter-desktop
 sudo snap install godot
 sudo apt install lmms
 
+
+sudo apt install php7
+sudo apt install mysql
+
 sudo apt install git
 sudo apt install npm
 sudo apt install pip && pip3
 sudo apt install cython
 sudo apt install nim
 sudo apt install emacs-25
+
+sudo npm install monogdb
 
