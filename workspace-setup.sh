698de1ac4d06d0861282b83de9b24f2cdd78abb9
burnsedia <bburnsed1995@gmail.com>
2020-11-03 11:54:43 -0500

    working on my project

--- workspace-setup.sh
+++ workspace-setup.sh
@@ -1,5 +1,7 @@
 #!usr/bin/bash
 
+
+# install software for workstation
 sudo snap install  blender
 sudo snap install krita
 sudo snap install gimp
@@ -13,12 +15,15 @@ sudo apt install lmms
 sudo apt install php7
 sudo apt install mysql
 
+# install dev tools
 sudo apt install git
 sudo apt install npm
 sudo apt install pip && pip3
 sudo apt install cython
 sudo apt install nim
 sudo apt install emacs-25
+sudo apt install neovim
+sudo apt install vim
 
 sudo npm install monogdb
 
